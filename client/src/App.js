871886f843096d65010dd33ce18ad84d8d170ae0
vchamale <victor.chamale@telusinternational.com>
2022-09-29 18:06:29 -0600

    pilot-1-budget-app

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Test from './Components/Test';
+
+function App() {
+  return (
+    <div className="App">
+      <Test />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/Components/Test.jsx
@@ -0,0 +1,7 @@
+function Test(){
+    return(
+        <div>Hola</div>
+    );
+}
+
+export default Test;

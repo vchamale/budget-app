7efb88a1603d03b2133b5e436389a37f6294a315
vchamale <victor.chamale@telusinternational.com>
2022-09-30 17:40:06 -0600

    added db file

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
 import Test from './Components/Test';
+import {Typography} from '@mui/material'
 
 function App() {
   return (
     <div className="App">
       <Test />
+      <Typography variant="h1"> Hello, World!</Typography>
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -1,3 +1,13 @@
+const express = require('express');
+const app = express();
+const port = 5000;
+
+app.listen(port, ()=>{
+    console.log('listening on port: ', port);
+});
+
+
+/*
 require('dotenv').config({path:".env"});
 
-console.log(process.env.JAMES);
+console.log(process.env.JAMES);*/
